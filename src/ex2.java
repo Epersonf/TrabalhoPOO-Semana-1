f25df2e6f1e6118c5e5028da4a6daebe3ce01c24
EPERSON <epersoncmxf@gmail.com>
2020-09-17 16:18:53 -0300

    Exercicio 2 feito

--- src/ex2.java
+++ src/ex2.java
@@ -1,7 +1,13 @@
 
 public class ex2 {
 	
+	public static void printMultiples(int n, int start, int end) {
+		int margin = (n - start % n);
+		for (int i = start + margin; i <= end; i += n)
+			System.out.println(i);
+	}
+	
 	public static void main(String[] args) {
-		
+		printMultiples(3, 0, 100);
 	}
 }
