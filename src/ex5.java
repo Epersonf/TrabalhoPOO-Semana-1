35769992efb828df515ee0ba80859c92705a100b
EPERSON <epersoncmxf@gmail.com>
2020-09-17 16:30:18 -0300

    Exercicio 5 feito

--- src/ex5.java
+++ src/ex5.java
@@ -1,8 +1,21 @@
 public class ex5 {
 	
+	public static void evaluate(int x) {
+		if (x == 1) return;
+		int newX = x;
+		
+		if (x % 2 == 0) newX = x/2;
+		if (x % 2 != 0) newX = 3*x + 1;
+		System.out.print(newX);
+		
+		if (newX != 1) System.out.print(" -> ");
+		else return;
+		
+		evaluate(newX);		
+	}	
 	
 	public static void main(String[] args) {
-		
+		evaluate(13);
 	}
 	
 }
