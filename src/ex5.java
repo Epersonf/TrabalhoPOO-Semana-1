da7738a10f723e24c425294029596de551a19a68
EPERSON <epersoncmxf@gmail.com>
2020-09-24 15:36:47 -0300

    Exercicio 5 simplificado

--- src/ex5.java
+++ src/ex5.java
@@ -3,17 +3,9 @@ import java.util.Scanner;
 public class ex5 {
 	
 	public static void evaluate(int x) {
-		if (x == 1) return;
-		int newX = x;
-		
-		if (x % 2 == 0) newX = x/2;
-		if (x % 2 != 0) newX = 3*x + 1;
-		System.out.print(newX);
-		
-		if (newX != 1) System.out.print(" -> ");
-		else return;
-		
-		evaluate(newX);		
+		System.out.print(x + " -> ");
+		if (x <= 1) return;
+		evaluate((x % 2==0) ? x/2 : (3*x+1) );	
 	}	
 	
 	public static void main(String[] args) {
